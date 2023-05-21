ebde0c521d4e47fd0182c618adb3dc2d180e88a3
Hashiko Hireo <hashikohireo@proton.me>
2023-05-21 02:38:59 -0600

    add ide settings

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+/*
+ * <wello-horld.rs: A hello world app (with a twist) written in Rust>
+ * Copyright (C) <2023> <Hashiko Hireo>
+ *
+ * This file is part of wello-horld.rs
+ *
+ * wello-horld.rs is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or any later version.
+ *
+ * wello-horld.rs is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with wello-horld.rs. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+fn main() {
+    println!("Hello, world!");
+}
